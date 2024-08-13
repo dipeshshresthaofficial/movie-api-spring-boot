bee5844affc4ef9aef99a6ad03bab8c3816f04e5
dipesh <dipeshshrestha0314@gmail.com>
2024-08-13 16:28:07 -0400

    added review document(entity)

--- src/main/java/dev/dipeshshrestha/moviesApi/documents/Review.java
+++ src/main/java/dev/dipeshshrestha/moviesApi/documents/Review.java
@@ -7,21 +7,13 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
-@Document(collection = "movies")
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Movie {
+public class Review {
     @Id
     private ObjectId id;
-    private String imdbId;
-    private String title;
-    private String releaseDate;
-    private String trailerLink;
-    private String poster;
-    private List<String> genres;
-    private List<String> backdrops;
-
+    private int ratings;
+    private String body;
 }
