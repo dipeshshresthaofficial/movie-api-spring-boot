8375393712184f590262d1096445b91db10e8e88
dipesh <dipeshshrestha0314@gmail.com>
2024-08-13 21:15:51 -0400

    updated return statements to return ResponseEntity object with JSON data and HTTP status code

--- src/main/java/dev/dipeshshrestha/moviesApi/controllers/MovieController.java
+++ src/main/java/dev/dipeshshrestha/moviesApi/controllers/MovieController.java
@@ -3,6 +3,8 @@ package dev.dipeshshrestha.moviesApi.controllers;
 import dev.dipeshshrestha.moviesApi.documents.Movie;
 import dev.dipeshshrestha.moviesApi.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,8 @@ public class MovieController {
     }
 
     @GetMapping("/")
-    public List<Movie> getAllMovies(){
-        return movieService.getAllMovies();
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
     }
 
     /**
@@ -30,7 +32,7 @@ public class MovieController {
      * @return Movie
      */
     @GetMapping("/{id}")
-    public Movie getSingleMovie(@PathVariable("id") String imdbId){
-        return movieService.getSingleMovie(imdbId);
+    public ResponseEntity<Movie> getSingleMovie(@PathVariable("id") String imdbId){
+        return new ResponseEntity<>(movieService.getSingleMovie(imdbId), HttpStatus.OK);
     }
 }
